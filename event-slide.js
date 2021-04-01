f1540ce4de94fe51814c49c23832156da82a858e
bny11638 <yaudev2601@gmail.com>
2021-03-31 21:09:29 -0400

    Am working on event stream.

---
+++ event-slide.js
@@ -0,0 +1,7 @@
+var event_info = [ 
+    {"title":"Event 1", "short": "This is event 1's short description!", "long": "This is event 1's super long description!", "img":"resources/flyer1", "date":"01/27/2021"}
+];
+
+function renderEventSlide() { 
+    
+}
