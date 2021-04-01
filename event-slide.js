8777a0839e379e53c6e7d10dfd4f1e2811940b02
bny11638 <yaudev2601@gmail.com>
2021-04-01 18:53:52 -0400

    Buttons expand when hover

--- event-slide.js
+++ event-slide.js
@@ -2,6 +2,8 @@ var event_info = [
     {"title":"Space Night", "short": "This is event 1's short description!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/space_04_01_21.PNG", "date":"04/01/21"},
     {"title":"Climate and CS", "short": "This is event 2's short description!", "long": "This is event 2's super long description!", "img":"resources/event_imgs/climate_cs_04-18-21.png", "date":"03/18/21"},
     {"title":"Blackrock @ ACM", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/blackrock_02_04_21.PNG", "date":"02/04/21"}
+
+
 ];
 
 function renderEventSlide() { 
