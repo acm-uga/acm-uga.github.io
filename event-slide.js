c18a854c3594c9099a96dd72c2fdaaf352d84e51
jefferyjohn <jefferyjohn2000@gmail.com>
2021-07-10 21:06:40 -0400

    add events

--- event-slide.js
+++ event-slide.js
@@ -2,6 +2,9 @@ var event_info = [
     {"title":"Space Night", "short": "This is event 1's short description!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/space_04_01_21.PNG", "date":"04/01/21"},
     {"title":"Climate and CS", "short": "This is event 2's short description!", "long": "This is event 2's super long description!", "img":"resources/event_imgs/climate_cs_04-18-21.png", "date":"03/18/21"},
     {"title":"Blackrock @ ACM", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/blackrock_02_04_21.png", "date":"02/04/21"}
+    {"title":"Research Panel", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_research.png", "date":"04/15/21"}
+    {"title":"UGAHacks @ ACM", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_ugahacks.png", "date":"01/21/21"}
+    {"title":"New Student Orientations", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_tabling.png", "date":"05/15/21"}
 
 
 ];
