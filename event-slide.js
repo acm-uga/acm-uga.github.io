274fc0fb864659fc6ec6f3c541d1e50a0f4121ea
jefferyjohn <jefferyjohn2000@gmail.com>
2021-07-10 21:16:39 -0400

    meeting short description updates

--- event-slide.js
+++ event-slide.js
@@ -1,10 +1,10 @@
 var event_info = [ 
-    {"title":"Space Night", "short": "This is event 1's short description!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/space_04_01_21.PNG", "date":"04/01/21"},
-    {"title":"Climate and CS", "short": "This is event 2's short description!", "long": "This is event 2's super long description!", "img":"resources/event_imgs/climate_cs_04-18-21.png", "date":"03/18/21"},
-    {"title":"Blackrock @ ACM", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/blackrock_02_04_21.png", "date":"02/04/21"},
-    {"title":"Research Panel", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_research.png", "date":"04/15/21"},
-    {"title":"UGAHacks @ ACM", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_ugahacks.png", "date":"01/21/21"},
-    {"title":"New Student Orientations", "short": "This is event 3's short description!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_tabling.png", "date":"05/15/21"}
+    {"title":"Space Night", "short": "A meeting w/ the Small Satellite Research Laboratory and Big Satellite Club!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/space_04_01_21.PNG", "date":"04/01/21"},
+    {"title":"Climate and CS", "short": "A look at UGA Climateology's WeatherSTEM API!", "long": "This is event 2's super long description!", "img":"resources/event_imgs/climate_cs_04-18-21.png", "date":"03/18/21"},
+    {"title":"Blackrock @ ACM", "short": "A UGAHacks pre-event featuring BlackRock!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/blackrock_02_04_21.png", "date":"02/04/21"},
+    {"title":"Research Panel", "short": "A research panel with professors and labs!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_research.png", "date":"04/15/21"},
+    {"title":"UGAHacks @ ACM", "short": "A UGAHacks team-building event!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_ugahacks.png", "date":"01/21/21"},
+    {"title":"New Student Orientations", "short": "Come find us at orientations!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_tabling.png", "date":"05/15/21"}
 
 
 ];
