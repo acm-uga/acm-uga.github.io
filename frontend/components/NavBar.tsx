8d3fc2656110b255d1fc10d4bef1e61ad397bc42
Matthew Tzou <70045875+MatzoMT@users.noreply.github.com>
2022-08-11 20:45:20 -0400

    added: mobile hamburger to nav bar

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -9,7 +9,7 @@ export const NavBar: React.FC<Props> = () => {
         <div>
           <h1 className="text-black">ACM</h1>
         </div>
-        <div>
+        <div className="hidden md:flex">
           <ul className="flex align-middle">
             <li className="ml-12">
               <a className="text-black visited:text-black" href="/">
@@ -38,6 +38,58 @@ export const NavBar: React.FC<Props> = () => {
             </li>
           </ul>
         </div>
+        <div className="md:hidden flex items-center">
+          <button className="outline-none mobile-menu-button">
+            <svg
+              className="w-6 h-6 text-black"
+              x-show="!showMenu"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
+        <div className="hidden mobile-menu">
+          <ul className="">
+            <li className="active">
+              <a
+                href="index.html"
+                className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#services"
+                className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
       <style jsx>
         {`
@@ -46,7 +98,6 @@ export const NavBar: React.FC<Props> = () => {
             width: 100%;
             height: 4em;
           }
-          
         `}
       </style>
     </div>
