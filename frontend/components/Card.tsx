2f5774bf62915875c7376eb360295adf0dce8df5
Matthew Tzou <70045875+MatzoMT@users.noreply.github.com>
2022-08-14 00:06:10 -0400

    updated: event cards

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 export const Card: React.FC<Props> = ({ title, date, description, imageSrc }) => {
   return (
-    <div className="max-w-xs shadow-lg text-center">
+    <div className="max-w-xs overflow-hidden shadow-lg text-center">
       <Image src={imageSrc} className="event-card-img" height={800} width={800} />
       <h1 className="font-medium">{title}</h1>
       <h1 className="border-b-4 border-bulldog-red">{date}</h1>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import { Card } from '../components/Card'
-import { NavBar } from '../components/NavBar'
+import Head from 'next/head';
+import { Card } from '../components/Card';
+import { EventCard } from '../components/EventCard';
+import { NavBar } from '../components/NavBar';
 
 const events = [
   {
@@ -46,16 +47,31 @@ export default function Home() {
       <NavBar />
       <div style={{ paddingTop: '4em' }}>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-        <div class="mx-28 grid grid-cols-4 gap-2">
+  <div class="p-10">  
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Mountain</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+      </div>
+    </div>
+  </div>
+        <div class="mx-12 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4">
           {events.map((event) => (
             <div>
-              <Card
+              <EventCard
                 title={event.title}
                 imageSrc={event.imageSrc}
                 description={event.description}
                 date={event.date}
               />
+
             </div>
           ))}
         </div>
