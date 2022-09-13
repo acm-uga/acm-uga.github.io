09ca391fab1907b2ae4d0a3c56058c35e419d43d
Matthew Tzou <70045875+MatzoMT@users.noreply.github.com>
2022-09-13 16:23:45 -0400

    added: calendar

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -194,6 +194,12 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <div className="mx-6 md:mx-16">
+          <h1 className="page-header text-center">Calendar</h1>
+          <iframe
+                    src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23616161&ctz=America%2FNew_York&showTabs=0&showPrint=0&showCalendars=0&showTz=0&showTitle=0&src=M2NnNTd0OGhmYWM3aWcxajRmZ3Btcm1kMThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23D81B60"
+                    style={{border:'solid 1px #ba0c2f'}} width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+        </div>
         <div>
           <h1 className="page-header text-center">Featured Events</h1>
           <div class="mx-12 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-6">
