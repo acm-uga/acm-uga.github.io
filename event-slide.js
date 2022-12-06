c7e8cc59a3594b530aeaa009ba67fc31f1eb412a
Dominarch <100043382+Dominarch@users.noreply.github.com>
2022-12-05 19:03:21 -0500

    worked on events and pictures

--- event-slide.js
+++ event-slide.js
@@ -10,10 +10,14 @@ var event_info = [
     {"title":"CSIP", "short": "Join us in Boyd 328 to learn more about how to ace your technical and behavioral interviews.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/csip.png", "date":"Every Tuesday at 6:00"},
     */
     {"title":"CSSIP", "short": "Come join us to prepare for your interviews in all things related from technical to behavioral.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/csip.png", "date":"Every Tuesday at 6:00"},
+    {"title":"Machine Learning Tensorflow", "short": "ACM and GDG Athens present a workshop on Machine Learning and Tensorflow.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Machine_Learning_and_Tensorflow_1 (1).png", "date":"11/17/22"},
+    {"title":"Figma Workshop", "short": "Learn to create stunning graphics and prototypes with Figma!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Figma_Workshop_22.png", "date":"11/10/22"},
+    {"title":"Makerspace Workshop", "short": "Learn how Makerspaces can bring your designs into reality!", "long":"This is event 1's super long description!", "img": "resources/event_imgs/makerspace22.png", "date":"11/03/22"},
+    {"title":"Compose Camp", "short": "ACM and GDG Athens teach you to create android UIs with Jetpack Compose!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Compose_Camp_2022.png", "date":"10/20/22"},    
     {"title":"Google @UGA", "short": "Uga Alumni from Google give vital tips to help you build your technical career!", "long": "This is event 2's super long description!", "img":"resources/event_imgs/climate_cs_04-18-21.png", "date":"09/28/22"},
     {"title":"Technical Interview Prep", "short": "CSIP and Google help prepare you for interviewing!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/blackrock_02_04_21.png", "date":"09/27/22"},
     {"title":"Hackathon Team-Building", "short": "UGAHacks and ACM help you prepare for the hackathon!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_research.png", "date":"09/22/22"},
-    {"title":"Google Cloud Workshop", "short": "ACM and GDG Athens give Google Cloud tips and tricks!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_ugahacks.png", "date":"09/15/22"},
+    {"title":"Tech Organizations Showcase", "short": "Learn about different tech organizations at UGA!", "long": "This is event 3's super long description!", "img":"resources/event_imgs/acm_tabling.png", "date":"08/25/22"}
 ];
 
 function renderEventSlide() { 
