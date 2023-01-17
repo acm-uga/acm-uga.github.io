c68d35897e02ee79d6a651eefc98b053a6ca47a9
lauren-bobo <ldb44912@uga.edu>
2023-01-17 13:25:36 -0500

    updated event

--- event-slide.js
+++ event-slide.js
@@ -9,13 +9,14 @@ var event_info = [
     {"title":"AT&T + ACM", "short": "Representatives from AT&T come to speak with us about life at AT&T and in their Technology Development Program!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/att.png", "date":"OCT 16, 2021"},
     {"title":"CSIP", "short": "Join us in Boyd 328 to learn more about how to ace your technical and behavioral interviews.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/csip.png", "date":"Every Tuesday at 6:00"},
     */
-   
+    
     {"title":"CSIP", "short": "Come join us to prepare for your interviews in all things related from technical to behavioral.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/csip.png", "date":"Every Tuesday at 6:00"},
+    {"title":"Full Stack Developement for Hackathons", "short": "Join ACM and UGAHacks for a crash course on full stack developement in preparation for the upcoming hackathon!", "long": "This is event 2's super long description!", "img":"resources/event_imgs/Full_Stack_Graphic.png", "date":"01/19/23"},
     {"title":"Machine Learning Tensorflow", "short": "ACM and GDG Athens present a workshop on Machine Learning and Tensorflow.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Machine_Learning_and_Tensorflow_1 (1).png", "date":"11/17/22"},
     {"title":"Figma Workshop", "short": "Learn to create stunning graphics and prototypes with Figma!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Figma_Workshop_22.png", "date":"11/10/22"},
     {"title":"Makerspace Workshop", "short": "Learn how Makerspaces can bring your designs into reality!", "long":"This is event 1's super long description!", "img": "resources/event_imgs/makerspace22.png", "date":"11/03/22"},
     {"title":"Compose Camp", "short": "ACM and GDG Athens teach you to create android UIs with Jetpack Compose!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Compose_Camp_2022.png", "date":"10/20/22"},    
-    {"title":"Game Night Social", "short": "Meet up with fellow ACM members for a night of videogames, card games, and board games! (+free food)", "long": "This is event 2's super long description!", "img":"resources/event_imgs/Game_night_social_7.png", "date":"09/29/22"},
+    
 ];
 
 function renderEventSlide() { 
