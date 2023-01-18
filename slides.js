ac1e2132db98a06c4243e55a0b1e6f0aa857d2f8
vansharora03 <vansh1993@icloud.com>
2023-01-18 13:25:36 -0500

    Fixed image slide to display images in order

--- slides.js
+++ slides.js
@@ -1,45 +1,32 @@
 
-var slideIndex = 1;
+var slideIndex = 0;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  slideIndex += n;
+  showSlides(slideIndex);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(n);
 }
 
 function showSlides(n) {
-  var i;
   var slides = document.getElementsByClassName("mySlides");
-  //var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  //check if n is above range
+  if (n >= slides.length) {slideIndex = 0}
+  //check if n is below range
+  if (n < 0) {slideIndex = slides.length - 1}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  /*
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  */
-  slides[slideIndex-1].style.display = "block";
-  //dots[slideIndex-1].className += " active";
-
+  slides[slideIndex].style.display = "block";
 }
 
 function render() { 
-  currentSlide(slideIndex);
-  if(slideIndex<3){
-    slideIndex++;
-  }else{
-    slideIndex=1;
-  }
-  setTimeout(render, 7000);
-
+  showSlides(slideIndex);
 }
 
 
