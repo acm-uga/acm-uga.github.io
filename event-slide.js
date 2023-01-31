76e766a4675dd30b699ba1ecf4f8599c426e49b5
lauren-bobo <ldb44912@uga.edu>
2023-01-31 14:37:36 -0500

    added research panel

--- event-slide.js
+++ event-slide.js
@@ -3,10 +3,11 @@ var event_info = [
     
     {"title":"CSIP", "short": "Come join us to prepare for your interviews in all things related from technical to behavioral.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/csip.png", "date":"Every Tuesday at 6:00PM"},
     {"title":"Open Source Project", "short": "Join us for our first ever open source project! Add to your resumè and create something great!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/open-source-project.png", "date":"Every Thursday at 7:00PM"},  
-    {"title":"Company Speaker: StateFarm", "short": "ACM is proud to host a guest speaker from StateFarm! Join us for exciting insight from the technology side of insurance buisness.", "long":"This is event 1's super long description!", "img": "resources/event_imgs/gmb-2-statefarm.png", "date":"1/26/23"},
+    {"title":"ACM Research Panel", "short": "Come to the research panel to discuss research oportunities at UGA. Ask questions and explore your specific research interests.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Research GBM 3 (2).png", "date":"02/09/23"},
+    {"title":"Company Speaker: StateFarm", "short": "ACM is proud to host a guest speaker from StateFarm! Join us for exciting insight from the technology side of insurance buisness.", "long":"This is event 1's super long description!", "img": "resources/event_imgs/gmb-2-statefarm.png", "date":"01/26/23"},
     {"title":"Full Stack Developement for Hackathons", "short": "Join ACM, GDG Athens, and UGAHacks for a crash course on full stack developement in preparation for the upcoming hackathon!", "long": "This is event 2's super long description!", "img":"resources/event_imgs/Full_Stack_Graphic.png", "date":"01/19/23"},
     {"title":"Machine Learning Tensorflow", "short": "ACM and GDG Athens present a workshop on Machine Learning and Tensorflow.", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Machine_Learning_and_Tensorflow_1 (1).png", "date":"11/17/22"},
-    {"title":"Figma Workshop", "short": "Learn to create stunning graphics and prototypes with Figma!", "long": "This is event 1's super long description!", "img":"resources/event_imgs/Figma_Workshop_22.png", "date":"11/10/22"},
+    
    
       
     
