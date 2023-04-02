a0a02f87c031c9a76a07c531815ea168adebf73c
vansharora03 <vansh1993@icloud.com>
2023-04-01 21:39:50 -0400

    Fixed sticky nav not working

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import { renderEventSlide } from "./scripts/event-slide";
 
 // Render slide
 renderEventSlide();
+
+// Hamburger menu
+const expandedMenu = document.querySelector('#expanded-menu');
+let opening = false;
+document.querySelector('body').addEventListener('click', (e) => {
+    if (!opening && expandedMenu.classList.contains('opened')) {
+        document.querySelector('#expanded-menu').classList.remove('opened');
+    }
+    opening = false;
+});
+document.querySelector('#hamburger').addEventListener('click', (e) => {
+    opening = true;
+    expandedMenu.classList.add('opened');
+});
