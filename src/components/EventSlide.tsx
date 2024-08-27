de222e7aa4308b948bcca73eefbe9c7b38972ac9
AdriGodaCS <acg44605@uga.edu>
2024-08-26 20:12:04 -0400

    Reverted event cards and filled white space

--- src/components/EventSlide.tsx
+++ src/components/EventSlide.tsx
@@ -11,6 +11,8 @@ type EventSlideProps = {
 const EventSlide = function({event, pos, onClick}: EventSlideProps) {
     return (<div onClick={onClick} className={`event-item ${pos}`}>
         <img src={event.img}></img>        
+        <p className="event-date">{event.date}</p>
+        <p className="event-short">{event.short}</p>
     </div>)
 }
 
