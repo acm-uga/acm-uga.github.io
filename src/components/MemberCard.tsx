c4558fc9db5d04fdbafb4d2d08d2fd7d50840d20
Cole Vaughn <colevaughn@s172-20-128-h170.paws.uga.edu>
2025-08-20 18:05:54 -0400

    Fixed linked-in logo

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -8,15 +8,22 @@ type MemberCardProps = {
 /** Renders an ACMExecutive member. */
 const MemberCard = function({member}: MemberCardProps) {
     return (
-        <div className="member-div">
+        <div className="member-div"
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+            }}
+        >
             <div className="officer-role">{member.role}</div>
             <div className="officer-name">{member.name}</div>
             <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
             <img 
-            className="linkedin-icon" 
-            src="https://cdn-icons-png.flaticon.com/512/174/174857.png" 
-            alt="LinkedIn Profile"
-            width={24} height={24}
+                className="linkedin-icon" 
+                src="https://img.icons8.com/?size=100&id=447&format=png&color=000000" 
+                alt="LinkedIn Profile"
+                width={28} height={28}
+                style={{ marginBottom: "8px" }}
             />
             </a>
             <img className="member-photo" src={member.img}></img>
